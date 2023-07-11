c97ba39b536ddacb8fe533019d8df219240b06f2
Danila Puzyrev <33790455+WitnessOfThe@users.noreply.github.com>
2023-07-11 21:28:28 +0100

    Adjustment for external transformations

--- GroundingDINO/groundingdino/util/inference.py
+++ GroundingDINO/groundingdino/util/inference.py
@@ -148,7 +148,7 @@ class Model:
         box_annotator = sv.BoxAnnotator()
         annotated_image = box_annotator.annotate(scene=image, detections=detections, labels=labels)
         """
-        processed_image = Model.preprocess_image(image_bgr=image).to(self.device)
+        processed_image = image.to(self.device)
         boxes, logits, phrases = predict(
             model=self.model,
             image=processed_image,
@@ -191,7 +191,7 @@ class Model:
         annotated_image = box_annotator.annotate(scene=image, detections=detections)
         """
         caption = ". ".join(classes)
-        processed_image = Model.preprocess_image(image_bgr=image).to(self.device)
+        processed_image = image.to(self.device)
         boxes, logits, phrases = predict(
             model=self.model,
             image=processed_image,
